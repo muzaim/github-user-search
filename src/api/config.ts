9fbc6fedf25a07c52753ff7a549284ea32310d38
Syafri Surya Muzaim <43945260+muzaim@users.noreply.github.com>
2025-06-04 08:16:56 +0700

    Feat : set vite and jest env

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
-export const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
-export const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
+// export const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
+// export const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
 
-// export const GITHUB_TOKEN = process.env.VITE_GITHUB_TOKEN || "";
-// export const GITHUB_URL = process.env.VITE_GITHUB_URL || "";
+export const GITHUB_TOKEN = process.env.VITE_GITHUB_TOKEN || "";
+export const GITHUB_URL = process.env.VITE_GITHUB_URL || "";
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		EnvironmentPlugin(["VITE_GITHUB_TOKEN", "VITE_GITHUB_URL"]),
+	],
 });
